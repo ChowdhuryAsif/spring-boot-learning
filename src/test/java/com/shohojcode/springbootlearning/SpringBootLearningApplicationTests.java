c47e83d65c02667f4eb16e1436a30fb0c92aefa8
Asif Ahmed Chowdhury <chowdhury.asif.cse@gmail.com>
2023-12-27 16:10:18 +0800

    Project Initiated

---
+++ src/test/java/com/shohojcode/springbootlearning/SpringBootLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shohojcode.springbootlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
